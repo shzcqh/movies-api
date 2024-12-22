c03ef8e97b04419f4c602ed1d1c04cf5b2cc9cf3
unknown <2867459421@qq.com>
2024-12-23 01:34:26 +0800

    Salt and Hash

--- api/users/userModel.js
+++ api/users/userModel.js
@@ -1,5 +1,5 @@
 import mongoose from 'mongoose';
-
+import bcrypt from 'bcrypt';
 const Schema = mongoose.Schema;
 
 const UserSchema = new Schema({
@@ -21,5 +21,26 @@ const UserSchema = new Schema({
     },
   },
 });
+UserSchema.methods.comparePassword = async function (passw) { 
+  return await bcrypt.compare(passw, this.password); 
+}
+UserSchema.statics.findByUserName = function (username) {
+  return this.findOne({ username: username });
+};
+UserSchema.pre('save', async function(next) {
+  const saltRounds = 10; // You can adjust the number of salt rounds
+  //const user = this;
+  if (this.isModified('password') || this.isNew) {
+    try {
+      const hash = await bcrypt.hash(this.password, saltRounds);
+      this.password = hash;
+      next();
+  } catch (error) {
+     next(error);
+  }
 
+  } else {
+      next();
+  }
+});
 export default mongoose.model('User', UserSchema);
